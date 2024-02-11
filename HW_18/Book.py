bc7e188a13f4db45f96292f573d90b03067f3c55
Diana Rtveladze <diana.rtveladze.1@btu.edu.ge>
2024-02-11 20:04:44 +0400

    add Homework 18

---
+++ HW_18/Book.py
@@ -0,0 +1,17 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __eq__(self, other):
+        if isinstance(other, Book):
+            return self.title == other.title and self.author == other.author
+        else:
+            return False
+
+book1 = Book('1984', 'George Orwell')
+book2 = Book('1984', 'George Orwell')
+book3 = Book('Brave New World', 'Aldous Huxley')
+
+print(book1 == book2)  # Output: True
+print(book1 == book3)  # Output: False
